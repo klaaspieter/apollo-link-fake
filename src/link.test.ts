ee838b5e81fa47fe0e97f44cfbfe3c1f4b85166d
Klaas Pieter Annema <kp@annema.me>
2020-03-20 16:46:45 +0100

    Support responding with arbitrary data

--- src/link.test.ts
+++ src/link.test.ts
@@ -3,27 +3,33 @@ import { execute } from "apollo-link";
 import gql from "graphql-tag";
 
 describe("MockLink", () => {
-  it("responds with mocked data", done => {
-    const query = gql`
-      query {
-        user {
-          name
-          age
-        }
-      }
-    `;
-    const link = new MockLink();
-
-    execute(link, { query }).subscribe({
-      next: result => {
-        expect(result).toEqual({
-          user: {
-            name: 'mock-value-for-field-"name"',
-            age: 'mock-value-for-field-"age"'
+  describe("resolveMostRecentOperation", () => {
+    it("can respond with mocked data", done => {
+      const query = gql`
+        query {
+          user {
+            name
+            age
           }
-        });
-        done();
-      }
+        }
+      `;
+      const link = new MockLink();
+      const result = {
+        user: {
+          name: "test",
+          age: 5
+        }
+      };
+
+      execute(link, { query }).subscribe({
+        next: r => {
+          expect(r).toEqual(result);
+        },
+        complete: () => {
+          done();
+        }
+      });
+      link.resolveMostRecentOperation(result);
     });
   });
 });
--- src/link.ts
+++ src/link.ts
@@ -1,17 +1,19 @@
-import { ApolloLink, Operation, Observable, FetchResult } from "apollo-link";
+import { ApolloLink, Operation, FetchResult } from "apollo-link";
+import Observable, { ZenObservable } from "zen-observable-ts";
 
 class MockLink extends ApolloLink {
+  private operations: ZenObservable.SubscriptionObserver<FetchResult>[] = [];
+
   public request(operation: Operation): Observable<FetchResult> {
-    return new Observable(observer => {
-      const result: any = {
-        user: {
-          name: 'mock-value-for-field-"name"',
-          age: 'mock-value-for-field-"age"'
-        }
-      };
-      observer.next(result);
-      observer.complete();
+    return new Observable<FetchResult>(observer => {
+      this.operations.push(observer);
     });
+  }
+
+  public resolveMostRecentOperation(data: Record<string, any>) {
+    const subscription = this.operations.pop();
+    subscription?.next(data);
+    subscription?.complete();
   }
 }
 export { MockLink };
