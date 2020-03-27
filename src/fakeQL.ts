8ea23f6bf8e9fbeb04e8210a67187fba2a50b8b5
Klaas Pieter Annema <kp@annema.me>
2020-03-27 13:58:54 +0100

    Support fakeQL resolvers argument

--- src/fakeQL.ts
+++ src/fakeQL.ts
@@ -1,6 +1,6 @@
-import { GraphQLSchema, IntrospectionQuery } from "graphql";
+import { GraphQLSchema, IntrospectionQuery, ValidationRule } from "graphql";
 import { Operation } from "apollo-link";
-import { fakeQL as originalFakeQL } from "fakeql";
+import { fakeQL as originalFakeQL, MockResolverMap } from "fakeql";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type Result = Record<string, any>;
@@ -8,9 +8,21 @@ type Result = Record<string, any>;
 export interface FakeQLProps {
   operation: Operation;
   schema: GraphQLSchema | IntrospectionQuery;
+  resolvers?: MockResolverMap;
+  validationRules?: ValidationRule[];
 }
-const fakeQL = ({ operation, schema }: FakeQLProps): Result => {
-  return originalFakeQL({ document: operation.query, schema });
+const fakeQL = ({
+  operation,
+  schema,
+  resolvers,
+  validationRules,
+}: FakeQLProps): Result => {
+  return originalFakeQL({
+    document: operation.query,
+    schema,
+    resolvers,
+    validationRules,
+  });
 };
 
 export { fakeQL };
